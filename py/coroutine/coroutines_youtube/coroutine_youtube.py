09e8c2efc7c90791f0b7b54909e2a1d10b8fbeed
wrq <pyq225@gmail.com>
2017-08-13 21:15:48 +0800

    add coroutine selector non_block

--- py/coroutine/coroutines_youtube/coroutine_youtube.py
+++ py/coroutine/coroutines_youtube/coroutine_youtube.py
@@ -8,40 +8,60 @@ import socket
 import time
 
 selector = DefaultSelector()
+n_tasks = 0
 
 
 def get(path):
+    global n_tasks
+    n_tasks += 1
     s = socket.socket()
     s.setblocking(False)
     try:
         s.connect(('localhost', 5000))
-    except:
+    except BlockingIOError:
         pass
 
     request = 'GET %s HTTP/1.0\r\n\r\n' % path
 
-    selector.register(s.fileno(), EVENT_WRITE)
+    callback = lambda: connected(s, request)
+    selector.register(s.fileno(), EVENT_WRITE, data=callback)
     selector.select()
-    selector.unregister(s.fileno())
 
+
+def connected(s, request):
+    selector.unregister(s.fileno())
     # s is writeable
     s.send(request.encode())
 
     chunks = []
-    while True:
-        selector.register(s.fileno(), EVENT_READ)
-        selector.select()
-        selector.unregister(s.fileno())
-        chunk = s.recv(1000)
-        if chunk:
-            chunks.append(chunk)
-        else:
-            body = (b''.join(chunks)).decode()
-            print(body.split('\n')[0])
-            return
+    callback = lambda: readable(s, chunks)
+    selector.register(s.fileno(), EVENT_READ, data=callback)
+
+
+def readable(s, chunks):
+    global n_tasks
+
+    # s is readable
+    selector.unregister(s.fileno())
+    chunk = s.recv(1000)
+    if chunk:
+        chunks.append(chunk)
+        callback = lambda: readable(s, chunks)
+        selector.register(s.fileno(), EVENT_READ, data=callback)
+    else:
+        body = (b''.join(chunks)).decode()
+        print(body.split('\n')[0])
+        n_tasks -= 1
 
 
 start = time.time()
 get('/foo')
 get('/foo')
+
+while n_tasks:
+    events = selector.select()
+    for event, mask in events:
+        cb = event.data
+        cb()
+
 print('took %.1f sec' % (time.time() - start))
