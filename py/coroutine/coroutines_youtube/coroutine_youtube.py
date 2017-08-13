84afafe96093f903958aa5d072fd2e9e69cf9b74
wrq <pyq225@gmail.com>
2017-08-13 20:54:21 +0800

    add coroutine selector

--- py/coroutine/coroutines_youtube/coroutine_youtube.py
+++ py/coroutine/coroutines_youtube/coroutine_youtube.py
@@ -2,20 +2,36 @@
 # https://www.youtube.com/watch?v=7sCu4gEjH5I&list=PLTOBJKrkhpoMdsT9RUERSDdEVrViykAEQ&index=1
 
 
+
+from selectors import DefaultSelector, EVENT_WRITE, EVENT_READ
 import socket
 import time
+
+selector = DefaultSelector()
 
 
 def get(path):
     s = socket.socket()
-    s.connect(('localhost', 5000))
+    s.setblocking(False)
+    try:
+        s.connect(('localhost', 5000))
+    except:
+        pass
 
     request = 'GET %s HTTP/1.0\r\n\r\n' % path
 
+    selector.register(s.fileno(), EVENT_WRITE)
+    selector.select()
+    selector.unregister(s.fileno())
+
+    # s is writeable
     s.send(request.encode())
 
     chunks = []
     while True:
+        selector.register(s.fileno(), EVENT_READ)
+        selector.select()
+        selector.unregister(s.fileno())
         chunk = s.recv(1000)
         if chunk:
             chunks.append(chunk)
